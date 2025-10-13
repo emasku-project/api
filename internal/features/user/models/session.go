403a0c299163191dcedb2871b3b4ba5d29dfa5fb
Rizal Anggoro <gnoogler4@gmail.com>
2025-10-13 10:45:29 +0700

    chore: setup ci

--- internal/features/user/models/session.go
+++ internal/features/user/models/session.go
@@ -1,11 +1,16 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Session struct {
 	gorm.Model
 
-	UserId uint   `json:"user_id"`
-	User   User   `json:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Token  string `json:"token"`
+	UserId       uint      `json:"user_id"`
+	User         User      `json:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Token        string    `json:"token"`
+	LastActiveAt time.Time `json:"last_active_at"`
 }
